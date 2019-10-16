60e20edc3ddf7685448c6554306c08cfee6b571d
Reetesh Kumar <reet1plus1@gmail.com>
2019-10-16 20:17:28 +0530

    Updates

---
+++ precache-manifest.b691744f4ecb375b41b7d55ed4107609.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c62356690f46106a3e6a4b65221bde4",
+    "url": "/ReactReduxWeather/index.html"
+  },
+  {
+    "revision": "80d4896e1c916f46ebe6",
+    "url": "/ReactReduxWeather/static/css/main.1bce351e.chunk.css"
+  },
+  {
+    "revision": "b4197cb886f0296c49b3",
+    "url": "/ReactReduxWeather/static/js/2.4817b5e5.chunk.js"
+  },
+  {
+    "revision": "80d4896e1c916f46ebe6",
+    "url": "/ReactReduxWeather/static/js/main.585cb79c.chunk.js"
+  },
+  {
+    "revision": "85980df93a84760daf83",
+    "url": "/ReactReduxWeather/static/js/runtime-main.00e7b1a5.js"
+  }
+]);
